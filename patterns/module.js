8c55062d698ed1ebbf47fddd00da7ff7fa98e29e
Sergey Popov <snaypssnp@gmail.com>
2016-08-13 09:34:43 +0300

    module es6

--- patterns/module.js
+++ patterns/module.js
@@ -1,42 +1,40 @@
 'use strict';
 
-var MYAPP = {};
+const MYAPP = {
+  namespace(nmString) {
+    let parts = nmString.split('.');
+    let self = this;
 
-MYAPP.namespace = function(nmString) {
-  var parts = nmString.split('.');
-  var self = this;
-  
-  for (var i = 0; i < parts.length; i++) {
-    if (typeof self[parts[i]] === 'undefined') {
-      self[parts[i]] = {};
-    } 
-    self = self[parts[i]];
+    parts.reduce((obj, item) => {
+      if (!(item in obj)) {
+        obj[item] = {};
+      }
+      return obj[item];
+    }, self);
+
+    return self;
   }
-  
-  return self;
-}
+};
+
 
 MYAPP.namespace('utilities.array');
 
 MYAPP.utilities.array = (function() {
 
-  var arrString = '[object Array]';
-  var toString  = Object.prototype.toString;
+  const arrString = '[object Array]';
+  const toString  = Object.prototype.toString;
+
+  const inArray = (neendle, haystack) => {
+    return haystack.some((item) => item === neendle);
+  }
+
+  const isArray = (a) => toString.call(a) === arrString;
 
   return {
-    inArray: function (neendle, haystack) {
+    inArray,
+    isArray
+  };
 
-      for (var i = 0, max = haystack.length; i < max; i++) {
-        if (haystack[i] === neendle) {
-          return true;
-        }
-      }
-      return false;
-    },
-    isArray: function(a) {
-      return toString.call(a) === arrString;
-    }
-  }
 }());
 
 console.log(MYAPP.utilities.array.inArray(1, [2,3,1,5]));
